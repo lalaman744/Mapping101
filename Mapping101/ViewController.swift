9712f58b4834f1177aa1cb2554861cc4afaf63c2
Josh Kleinschmidt <tf8546hu@t3040-2-6.local>
2019-02-12 18:18:07 -0600

    Initial commit

--- Mapping101/ViewController.swift
+++ Mapping101/ViewController.swift
@@ -7,14 +7,36 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
     }
 
-
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            mapView.showsUserLocation = true
+            moveToCurrentLocation()
+        } else {
+            let alert = UIAlertController(title: "Can't display location", message: "Please grant permission in settings", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK",
+                                          style: .default,
+                                          handler: {(action: UIAlertAction) -> Void in UIApplication.shared.open(URL(string:UIApplication.openSettingsURLString)!)}))
+            present(alert, animated: true, completion: nil)
+        }
+    }
+    func moveToCurrentLocation() {
+        if let location = locationManager.location {
+            mapView.setCenter(location.coordinate, animated: true)
+        }
+    }
 }
 
